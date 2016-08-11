2fa5bbf959264e456053e02d7193f300e5fe01ea
RainChen <hirainchen@gmail.com>
2016-08-12 00:56:08 +0800

    add test: when run in production/staging env should compute assets path

--- spec/react/rails/img_js_features_spec.rb
+++ spec/react/rails/img_js_features_spec.rb
@@ -41,4 +41,19 @@ describe 'js features', type: :feature, js: true do
     expect(page).to have_content 'reactjs logo'
     expect(page).to have_selector 'img[class="logo"][src="/assets/logo/reactjs-logo.svg"]'
   end
+
+  context "when run in production/staging env" do
+    around do |example|
+      Rails.env = 'staging' # simulate running in staging env
+      example.run
+      Rails.env = 'test'
+    end
+
+    it "should compute assets path" do
+      visit '/posts/show?debug_assets=true' # passing debug_assets=true to expand assets links
+
+      expect(page).to have_content 'rails logo(100)'
+      expect(page).to have_selector 'img[width="100"][src="/assets/rails-logo-e6d7efc676da55da20de9da9e4d231a44542d457e0191a3a1074a945bb194363.svg"]'
+    end
+  end
 end
